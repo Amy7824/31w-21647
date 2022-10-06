1e1f628d5ac0fb52c3a7680327af4b99b7be68e9
ZHI <78247487@qq.com>
2022-10-06 12:38:16 -0400

    étape4-creer aside menu avec le fichier nav_aside

--- header.php
+++ header.php
@@ -47,4 +47,10 @@
 	</header><!-- #masthead -->
 	<aside class="widget__area">
 		<h2>Menu Secondaire</h2>
+		<?php wp_nav_menu(array(
+			"menu" => "aside",
+			"container" => "nav",
+			"container_class" => "menu__aside"
+		));
+	    ?>
 	</aside>
